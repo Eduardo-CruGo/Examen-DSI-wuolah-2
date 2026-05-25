1ed3c55d6ffae01d74ad6c3ce1fcb6dd2e9bc032
Eduardo-CruGo <alu0101432748@ull.edu.es>
2026-05-25 17:24:29 +0100

    early deploy

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import "./db/mongoose.js";
-//import { lectorRouter } from "./routers/lector_router.js";
+import { lectorRouter } from "./routers/lector_router.js";
+import { libroRouter } from "./routers/libro_router.js";
+import { defaultRouter } from "./routers/default_router.js";
 
 
 export const app = express();
 app.use(express.json());
-//app.use(lectorRouter);
+app.use(lectorRouter);
+app.use(libroRouter);
+app.use(defaultRouter);
